7c996497c54cea4c687a65a1e3b1d50b5db8d678
K2222810 <axelbaynes@gmail.com>
2026-02-10 16:40:54 +0000

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,8 @@ import { useContext } from 'react';
 import { Link } from "react-router";
 
 import { UserContext } from '../../contexts/UserContext';
+import styles from './NavBar.module.css';
+import Logo from '../../assets/images/logo-h.svg';
 
 const NavBar = () => {
 const { user, setUser } = useContext(UserContext);
@@ -12,17 +14,16 @@ const handleSignOut = () => {
   };
 
   return (
-    <nav>
+    <nav className={styles.container}>
+      <Link to='/'><img src={Logo} alt='A cute owl' /></Link>
       {user ? (
         <ul>
-          <li><Link to='/'>Dashboard</Link></li>
           <li><Link to='/applications'>Applications</Link></li>
           <li><Link to='/follow-ups'>Follow Ups</Link></li>
           <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
         </ul>
       ) : (
         <ul>
-            <li><Link to='/'>Home</Link></li>
             <li><Link to="/sign-in">Sign In</Link></li>
             <li><Link to='/sign-up'>Sign Up</Link></li>
         </ul>
